0f5473e677133b7df4d121ac8af5f5941129a050
Michał Pilarski <misioskiper@gmail.com>
2019-03-29 16:47:31 +0100

    wygaszacz

---
+++ Wygaszacz/wygaszacz.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Wygaszacz
+{
+    partial class wygaszacz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // wygaszacz
+            // 
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "wygaszacz";
+            this.Text = "Wyświetlacz";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Wygaszacz_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Wygaszacz_KeyDown);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Wygaszacz_MouseClick);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Wygaszacz_MouseMove);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Wygaszacz/wygaszacz.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Wygaszacz
+{
+    public partial class wygaszacz : Form
+    {
+        public wygaszacz()
+        {
+            InitializeComponent();
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string[] Pictures { get; set; }
+        public int CurrentPicture { get; set; }
+        private const string pictureFolder= @"D:\obrazy";
+
+        private void SetMousePosition()
+        {
+            X = Cursor.Position.X;
+            Y = Cursor.Position.Y;
+        }
+
+        private void LoadPicturesFromTheFolder(string folderPath)
+        {
+            try
+            {
+                Pictures = Directory.GetFiles(folderPath);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show($"Błąd w trakcie pobierania plików z {folderPath}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            if(Pictures.Length == 0)
+            {
+                MessageBox.Show($"W folderze {folderPath} nie ma plików", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SetBackgroundImage(int pos)
+        {
+            try
+            {
+                this.BackgroundImage = Image.FromFile(Pictures[pos]);
+                CurrentPicture = pos;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show($"Nie udało się załadować obrazka {Pictures[pos]}", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            var lastPicture = Pictures.Length - 1;
+
+            if (CurrentPicture == lastPicture)
+            {
+                SetBackgroundImage(0);
+            }
+            else
+            {
+                SetBackgroundImage(CurrentPicture + 1);
+            }
+        }
+
+        private void Wygaszacz_MouseClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Wygaszacz_KeyDown(object sender, KeyEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Wygaszacz_MouseMove(object sender, MouseEventArgs e)
+        {
+            var x = Cursor.Position.X;
+            var y = Cursor.Position.Y;
+
+            if (x != X || y != Y)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Wygaszacz_Load(object sender, EventArgs e)
+        {
+            SetMousePosition();
+            LoadPicturesFromTheFolder(pictureFolder);
+            SetBackgroundImage(0);
+            Cursor.Hide();
+            this.WindowState = FormWindowState.Maximized;
+            timer1.Start();
+        }
+    }
+}
